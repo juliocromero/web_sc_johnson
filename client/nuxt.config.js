f60260d9f65ae503da01c63054a73ef39cad01ef
unknown <yonar.ramos@systelec.com>
2021-07-21 09:01:53 -0300

    nuxt_config

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -18,7 +18,7 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
-    BeforeUnloadEvent:{
+    closed:{
       src: "@/middleware/DESLOGUIN.js",
     }
   },
